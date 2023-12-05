224f9eeb0e6283a8be60807c96d7b08575dcbe72
MateusFoppa <55770663+MateusFoppa@users.noreply.github.com>
2023-12-05 01:12:56 -0300

    :✨: feat: Add simbol

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { sse, myEmitter } = require('./src/sse');
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/events',sse, (req, res) => {
+app.get('/events', sse, (req, res) => {
   res.send('Hello World!')
 })
 app.get('/', (req, res) => {
@@ -20,23 +20,23 @@ app.get('/partida', (req, res) => {
   // Lê o conteúdo do arquivo HTML e o envia como resposta
   res.sendFile(`${__dirname}/pages/partida.html`);
 });
-app.post('/bingo', (req,res)=>{
-const nomeJogador = req.body.nomeJogador ?? "";
-try {
-  iniciarSorteio(nomeJogador);
-  myEmitter.emit('bingo_vencedor',nomeJogador);
-} catch (error) {
-  console.error('Erro:', error.message);
-  res.status(500).send('Algo deu errado!');
-}
-
-
+app.post('/bingo', (req, res) => {
+  const nomeJogador = req.body.nomeJogador ?? "";
+  try {
+    iniciarSorteio(nomeJogador);
+    myEmitter.emit('bingo_vencedor', nomeJogador);
+  } catch (error) {
+    console.error('Erro:', error.message);
+    res.status(500).send('Algo deu errado!');
+  }
   res.send('Alguem bingou!')
 })
 
-app.post('/iniciar_sorteio', (req,res)=>{
+app.get('/iniciar_sorteio', (req, res) => {
   try {
+    myEmitter.emit('bingo_iniciado');
     iniciarSorteio(false)
+
   } catch (error) {
     console.error('Erro:', error.message);
     res.status(500).send('Algo deu errado!');
